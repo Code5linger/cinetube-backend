5794db0919ec55082ce132bad2031c5c7387a17e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-03-31 23:09:18 +0600

    Auth Fixed
    Auth Fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,25 +5,40 @@ import { config } from '../config/config.js';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: 'postgresql' }),
+
   secret: config.BETTER_AUTH_SECRET,
+
   baseURL: config.BETTER_AUTH_URL,
   basePath: '/api/better-auth',
+
   trustedOrigins: [
     'http://localhost:3000',
     'http://localhost:5000',
-    config.FRONTEND_URL,
+    'https://cinetube-frontend-5zoh.vercel.app',
   ],
+
   session: {
     expiresIn: Number(config.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN),
     updateAge: Number(config.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE),
+
+    cookieOptions: {
+      secure: true,
+      sameSite: 'none',
+      httpOnly: true,
+    },
   },
-  emailAndPassword: { enabled: true },
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
   socialProviders: {
     google: {
       clientId: config.GOOGLE_CLIENT_ID,
       clientSecret: config.GOOGLE_CLIENT_SECRET,
     },
   },
+
   user: {
     additionalFields: {
       role: { type: 'string', defaultValue: 'USER' },
